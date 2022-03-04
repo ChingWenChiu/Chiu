fa313f5cb42647dddcea3137db764117bbeb1657
ChingWenChiu <62386708+ChingWenChiu@users.noreply.github.com>
2022-03-04 18:03:18 +0800

    chiu

--- same.js
+++ same.js
@@ -3,5 +3,5 @@ var top = document.getElementById("top");
 function pageScroll() {
     window.scrollBy(0,-window.innerHeight);
     scrolldelay = setTimeout("pageScroll()",100);
-    if(document.documentElement.scrollTop==0) clearTimeout(scrolldelay);
+    if(document.documentElement.scrollTop == 0) clearTimeout(scrolldelay);
 }
